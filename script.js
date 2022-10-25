5708371567a330c51fe99a98014b04268ec65728
Kaushik G Bhat <kaushikgbhat@Kaushiks-Mac-mini.local>
2022-10-25 12:26:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  let body = document.querySelector("body");
+  let result = document.querySelector("#result");
+
+  let dark_mode_btn = document.querySelector(".dark_mode_btn");
+  let clear = document.querySelector("#clear");
+  let history = document.querySelector("#history");
+  let equalTo = document.querySelector("#equalTo");
+  let delete_single_num = document.querySelector("#delete_single_num");
+
+  let Normal_btn = document.querySelectorAll("#Normal_btn");
+
+  let initial_value = "";
+
+  let operators = ["+", "-", ".", "*", "/"];
+
+  Normal_btn.forEach((Normal_btn, index) => {
+    Normal_btn.addEventListener("click", function () {
+      let text = this.innerHTML;
+      if (operators.includes(text)) {
+        if (operators.some((e) => result.innerHTML.includes(e))) {
+          return;
+        }
+      }
+      initial_value += text;
+      result.innerHTML = initial_value;
+    });
+  });
+
+  /*equal to button action*/
+  equalTo.addEventListener("click", function () {
+    if (result.innerHTML != "") {
+      history.innerHTML = result.innerHTML;
+      result.innerHTML = eval(result.innerHTML);
+      initial_value = eval(result.innerHTML);
+    } else {
+      alert("please enter Number");
+    }
+  });
+
+  /*clear all number*/
+  clear.addEventListener("click", function () {
+    result.innerHTML = "";
+    initial_value = "";
+  });
+
+  /*delete single number*/
+  delete_single_num.addEventListener("click", function () {
+    result.innerHTML = result.innerHTML.substring(
+      0,
+      result.innerHTML.length - 1
+    );
+    initial_value = result.innerHTML;
+  });
+});
